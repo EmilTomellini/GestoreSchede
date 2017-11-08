8c7a7fce74a2c9234ca9574e4f555b90fd43e6a1
RicciLorenzo <lorenzorik@gmail.com>
2017-11-08 18:36:05 +0100

    update

---
+++ src/controller/HomeServlet.java
@@ -0,0 +1,50 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = { "/home"})
+public class HomeServlet extends HttpServlet {
+
+
+    public HomeServlet() {
+        super();
+    }
+
+	/**
+	 * 
+     * @param request
+     * @param response
+     * @throws javax.servlet.ServletException
+     * @throws java.io.IOException
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/jsp/index.html");
+
+		dispatcher.forward(request, response);
+		
+	}
+
+	/**
+	 * 
+     * @param request
+     * @param response
+     * @throws javax.servlet.ServletException
+     * @throws java.io.IOException
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+    @Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
